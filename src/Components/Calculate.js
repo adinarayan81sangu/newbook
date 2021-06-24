16ecd750f3c967318c8c9fead7aada71c868daeb
adinarayan81sangu <sanguadinarayana@gmail.com>
2021-06-24 12:20:48 +0530

    second-commit

---
+++ src/Components/Calculate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+ class  extends Component {
+    render() {
+        return (
+            <div>
+                hello   we are main component
+            </div>
+        )
+    }
+}
